d0265a00adf7aca6c7334ce05cd685995aa86c14
Beth Mitchell <beth.mitchell20@gmail.com>
2014-12-28 22:27:59 -0800

    Fixes to drone.js

--- demos/Drone/drone.js
+++ demos/Drone/drone.js
@@ -2,25 +2,19 @@
 
 goog.require('Blockly.JavaScript');
 
-var arDrone = require('ar-drone');
-var client  = arDrone.createClient();
-
 Blockly.JavaScript['drone_control'] = function(block) {
-    var statements_name = Blockly.JavaScript.statementToCode(block, 'program');
     // This code causes the drone to take off.
     var code = 'client.takeoff()';
+    code += '\n' + Blockly.JavaScript.statementToCode(block, 'PROGRAM');
     return code;
 };
 
 Blockly.JavaScript['drone_up'] = function(block) {
   // This function causes the drone to gain altitude for 3 seconds at quarter speed.
-    var code = 'client
-.after(250, function() {
-this.up(0.25);
-})
-.after(3000, function() {
-this.stop();
-});';
+    var code = 'client.up(0.25)\n\
+      .after(3000, function() {\n\
+        this.stop();\n\
+      });';
     return code;
 };
 
